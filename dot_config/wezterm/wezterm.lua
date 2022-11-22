1a157f5752b9486f625c23f0dd0a345c624dbfa1
backwardspy <backwardspy@gmail.com>
2022-11-22 16:51:06 +0000

    wezterm: font & tab bar tweaks

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -5,6 +5,8 @@ return {
         "Fantasque Sans Mono",
         "Apple Color Emoji"
     }),
-    font_size = 15,
+    font_size = 16,
     color_scheme = "Catppuccin Mocha",
+    use_fancy_tab_bar = false,
+    hide_tab_bar_if_only_one_tab = true,
 }
