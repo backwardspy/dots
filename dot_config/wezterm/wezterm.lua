3bb2c351a46457d7c3585c8d64bf94ff9f12eb96
backwardspy <backwardspy@gmail.com>
2022-11-22 23:23:40 +0000

    wezterm: ctrl+click for links

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -3,10 +3,25 @@ local wezterm = require("wezterm")
 return {
     font = wezterm.font_with_fallback({
         "Fantasque Sans Mono",
-        "Apple Color Emoji"
+        "Apple Color Emoji",
     }),
     font_size = 16,
     color_scheme = "Catppuccin Mocha",
     use_fancy_tab_bar = false,
     hide_tab_bar_if_only_one_tab = true,
+    mouse_bindings = {
+        -- only select with lmb, don't open links
+        {
+            event = { Up = { streak = 1, button = "Left" } },
+            mods = "NONE",
+            action = wezterm.action.CompleteSelection("PrimarySelection"),
+        },
+
+        -- ctrl+lmb for links
+        {
+            event = { Up = { streak = 1, button = "Left" } },
+            mods = "CTRL",
+            action = wezterm.action.OpenLinkAtMouseCursor,
+        },
+    },
 }
