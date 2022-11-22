3259c3f9917df1f609aaa19b95eecb21639cfe04
backwardspy <backwardspy@gmail.com>
2022-11-22 15:36:40 +0000

    wezterm: add conf

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require("wezterm")
+
+return {
+    font = wezterm.font_with_fallback({
+        "Fantasque Sans Mono",
+        "Apple Color Emoji"
+    }),
+    font_size = 15,
+    color_scheme = "Catppuccin Mocha",
+}
