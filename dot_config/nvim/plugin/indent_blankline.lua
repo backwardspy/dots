155fe3477e8dbf66ece337409aa1efba5af4b609
backwardspy <backwardspy@gmail.com>
2022-12-01 23:34:15 +0000

    x11: add i3/picom/dunst configs for x11 alt sessions

--- dot_config/nvim/plugin/indent_blankline.lua
+++ dot_config/nvim/plugin/indent_blankline.lua
@@ -3,14 +3,4 @@ if not ok then
     return
 end
 
-indent_blankline.setup({
-    space_char_blankline = " ",
-    char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-        "IndentBlanklineIndent3",
-        "IndentBlanklineIndent4",
-        "IndentBlanklineIndent5",
-        "IndentBlanklineIndent6",
-    },
-})
+indent_blankline.setup()
