4b5083a1284dc05f05c6ef1c0c15ed5f758f6398
backwardspy <backwardspy@gmail.com>
2022-12-21 19:31:32 +0000

    nvim: switch from packer to lazy

--- dot_config/nvim/after/plugin/executor.lua
+++
@@ -1,4 +0,0 @@
-require("executor").setup({})
-vim.keymap.set("n", "<leader>er", vim.cmd.ExecutorRun)
-vim.keymap.set("n", "<leader>ec", vim.cmd.ExecutorSetCommand)
-vim.keymap.set("n", "<leader>ev", vim.cmd.ExecutorToggle)
