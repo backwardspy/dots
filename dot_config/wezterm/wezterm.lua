f1d8c5f997fb25d05ea62d684f99932ca993d6b9
backwardspy <backwardspy@gmail.com>
2023-01-03 17:20:29 +0000

    wezterm: 0 padding life

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,23 +1,12 @@
 local wezterm = require("wezterm")
 
-wezterm.on("update-status", function(window, pane)
-    if pane:get_foreground_process_info().name == "nvim" then
-        window:set_config_overrides({
-            window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
-        })
-    else
-        window:set_config_overrides({
-            window_padding = { left = 8, right = 8, top = 8, bottom = 8 },
-        })
-    end
-end)
-
 return {
     font = wezterm.font("Fantasque Sans Mono", { weight = "Bold" }),
     font_size = string.match(wezterm.target_triple, "darwin") and 15 or 13,
     color_scheme = "Catppuccin Mocha",
     use_fancy_tab_bar = false,
     hide_tab_bar_if_only_one_tab = true,
+    window_padding = { top = 0, bottom = 0, left = 0, right = 0 },
     mouse_bindings = {
         -- only select with lmb, don't open links
         {
