537e4981a5d7ccab9fc8e474984111a12e777657
backwardspy <backwardspy@gmail.com>
2023-01-07 11:35:10 +0000

    nvim: replace some plugins with mini

---
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "echasnovski/mini.nvim",
+        config = function()
+            -- add gc/gcc actions, gc textobject
+            require("mini.comment").setup({})
+
+            -- highlight word under cursor
+            require("mini.cursorword").setup({ delay = 0 })
+
+            -- indent lines, `i` textobject, `]i`/`[i` motions
+            require("mini.indentscope").setup({
+                draw = { delay = 0 },
+                symbol = "│",
+            })
+
+            -- autopairs
+            require("mini.pairs").setup({})
+        end,
+    }
+}
