bda5ed6da9c7b6c6dfb5daf5867864cf3825ec4d
backwardspy <backwardspy@pigeon.life>
2023-01-24 14:14:27 +0000

    wezterm: blur background in plasma

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,5 +1,13 @@
 local wezterm = require("wezterm")
 
+if string.match(wezterm.target_triple, "linux") then
+    wezterm.on("window-focus-changed", function()
+        os.execute(
+            "xdotool search -classname org.wezfurlong.wezterm | xargs -I{} xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -id {}"
+        )
+    end)
+end
+
 return {
     font = wezterm.font("Rec Mono Duotone"),
     font_size = string.match(wezterm.target_triple, "darwin") and 15 or 13,
@@ -7,12 +15,13 @@ return {
     use_fancy_tab_bar = false,
     hide_tab_bar_if_only_one_tab = true,
     window_padding = { top = 0, bottom = 0, left = 0, right = 0 },
+    window_background_opacity = 0.8,
     keys = {
         {
             key = "f",
             mods = "SHIFT|CTRL",
             action = wezterm.action.ToggleFullScreen,
-        }
+        },
     },
     mouse_bindings = {
         -- only select with lmb, don't open links
