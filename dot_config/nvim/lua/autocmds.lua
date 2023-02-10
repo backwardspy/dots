870c4dab864edf5c02eac4503980a6cd9a2dde9b
backwardspy <backwardspy@pigeon.life>
2023-02-10 20:51:02 +0000

    nvim: this feels like a terrible idea

---
+++ dot_config/nvim/lua/autocmds.lua
@@ -0,0 +1,1 @@
+vim.api.nvim_create_autocmd("CmdWinEnter", { command = "startinsert" })
--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -1,5 +1,3 @@
-vim.g.mapleader = " "
-
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -8,4 +6,9 @@ vim.opt.expandtab = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.clipboard = "unnamedplus"
+vim.opt.cmdheight = 0
+
+if vim.g.vscode then
+    vim.opt.clipboard = "unnamedplus"
+end
+
