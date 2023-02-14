8be87847de10cc95b54dd986a7e752ad8c531249
backwardspy <backwardspy@pigeon.life>
2023-02-14 19:15:07 +0000

    nvim: tweak aesthetics and add a couple of helpful telescope binds

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,37 @@
 return {
-  {
-    "LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
+    {
+        "LazyVim",
+        opts = {
+            colorscheme = "catppuccin",
+        },
     },
-  },
+    {
+        "catppuccin",
+        opts = {
+            custom_highlights = function(colors)
+              return {
+                  -- telescope
+                  TelescopeMatching = { fg = colors.yellow },
+                  TelescopeSelection = { fg = colors.text, bg = colors.surface0 },
+                  -- results
+                  TelescopeResultsNormal = { bg = colors.mantle },
+                  TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
+                  TelescopeResultsTitle = { fg = colors.mantle },
+                  -- prompt
+                  TelescopePromptNormal = { bg = colors.surface0 },
+                  TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
+                  TelescopePromptTitle = { bg = colors.teal, fg = colors.mantle },
+                  TelescopePromptPrefix = { bg = colors.surface0 },
+                  -- preview
+                  TelescopePreviewNormal = { bg = colors.crust },
+                  TelescopePreviewBorder = { bg = colors.crust, fg = colors.crust },
+                  TelescopePreviewTitle = { bg = colors.pink, fg = colors.mantle },
+                  --
+                  -- neotree
+                  --
+                  NeotreeNormal = { bg = colors.mantle },
+              }
+            end
+        }
+    }
 }
