3cb51d862fe2e760944501915468185612e2e138
backwardspy <backwardspy@pigeon.life>
2023-02-15 12:10:01 +0000

    nvim: alpha tweaks

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -10,7 +10,9 @@ return {
         opts = {
             custom_highlights = function(colors)
               return {
+                  --
                   -- telescope
+                  --
                   TelescopeMatching = { fg = colors.yellow },
                   TelescopeSelection = { fg = colors.text, bg = colors.surface0 },
                   -- results
@@ -30,6 +32,17 @@ return {
                   -- neotree
                   --
                   NeotreeNormal = { bg = colors.mantle },
+                  --
+                  -- alpha
+                  --
+                  DashboardHeader1 = { fg = "#94E2D5" },
+                  DashboardHeader2 = { fg = "#A2DDD8" },
+                  DashboardHeader3 = { fg = "#B0D9DA" },
+                  DashboardHeader4 = { fg = "#BED4DD" },
+                  DashboardHeader5 = { fg = "#CBD0DF" },
+                  DashboardHeader6 = { fg = "#D9CBE2" },
+                  DashboardHeader7 = { fg = "#E7C7E4" },
+                  DashboardHeader8 = { fg = "#F5C2E7" },
               }
             end
         }
