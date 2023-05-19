fbafb58f8fe479166b4bf73642e87b15aaa94c1e
backwardspy <backwardspy@pigeon.life>
2023-05-19 23:17:12 +0100

    pkgs: make c++ stdlib available to nvim

--- configs/neovim.nix
+++ configs/neovim.nix
@@ -1,4 +1,8 @@
-{config, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   programs.neovim = {
     enable = true;
     defaultEditor = true;
@@ -13,4 +17,7 @@
     source = config.lib.file.mkOutOfStoreSymlink ../ext/neovim;
     recursive = true;
   };
+
+  # lots of treesitter parsers need the C++ stdlib
+  programs.fish.functions = {nvim = "env LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib ${pkgs.neovim}/bin/nvim $argv";};
 }
