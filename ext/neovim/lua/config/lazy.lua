200e553d9b219e213678274356effb4bbaa606d8
backwardspy <backwardspy@pigeon.life>
2023-05-20 16:29:39 +0100

    nvim: add python & rust lazyvim configs

--- ext/neovim/lua/config/lazy.lua
+++ ext/neovim/lua/config/lazy.lua
@@ -7,10 +7,15 @@ end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
+  dev = {
+    path = "~/src/backwardspy",
+    patterns = { "backwardspy" },
+    fallback = true,
+  },
   spec = {
     {
       "LazyVim/LazyVim",
-      import = "lazyvim.plugins"
+      import = "lazyvim.plugins",
     },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.dap.core" },
@@ -18,6 +23,11 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.util.project" },
+
+    "backwardspy/lazyvim-pigeon-configs",
+    { import = "pigeon_conf.lang.rust" },
+    { import = "pigeon_conf.lang.python" },
+
     { import = "plugins" },
   },
   defaults = {
