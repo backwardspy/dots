60c408163fa8b38d8da06aa7ad18abfeeb8fe0ad
backwardspy <backwardspy@pigeon.life>
2023-05-22 09:28:12 +0100

    nvim: add catppuccin colour overrides

---
+++ ext/neovim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#080808",
+          crust = "#0B0B0B",
+        },
+      },
+    },
+  },
+}
