1aaa395be1d3147ecb4c63464732f12066f66ed5
backwardspy <backwardspy@pigeon.life>
2023-05-26 09:55:07 +0100

    feat: add git push/push binds

--- ext/neovim/lua/plugins/editor.lua
+++ ext/neovim/lua/plugins/editor.lua
@@ -6,6 +6,8 @@ return {
     keys = {
       { "<leader>gg", "<cmd>Git<cr>", desc = "Git Status" },
       { "<leader>gG", "<cmd>GBrowse<cr>", desc = "Git Browse" },
+      { "<leader>gP", "<cmd>Git push<cr>", desc = "Git Push" },
+      { "<leader>gp", "<cmd>Git pull<cr>", desc = "Git Pull" },
     },
     ft = "gitcommit",
   },
