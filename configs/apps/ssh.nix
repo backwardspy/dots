64257fc7cdad769942e5a0b6bf1ac4ed0ae126f8
backwardspy <backwardspy@pigeon.life>
2023-06-29 12:12:18 +0100

    ssh: add keys to keychain

--- configs/apps/ssh.nix
+++ configs/apps/ssh.nix
@@ -1,11 +1,18 @@
-{...}: {
-  programs.keychain.enable = true;
+{homeDirectory, ...}: {
+  programs.keychain = {
+    enable = true;
+    keys = [
+      "${homeDirectory}/.ssh/id_rsa"
+      "${homeDirectory}/.ssh/id_ed25519"
+    ];
+  };
 
   programs.ssh = {
     enable = true;
     extraConfig = ''
       AddKeysToAgent yes
       UseKeychain yes
+      IdentityFile ${homeDirectory}/.ssh/id_ed25519
     '';
   };
 }
