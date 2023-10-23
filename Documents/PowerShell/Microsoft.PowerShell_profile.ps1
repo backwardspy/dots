fd8d27e9ed130847a012543c3ef597d374fca5dc
backwardspy <backwardspy@pigeon.life>
2023-10-23 13:01:24 +0100

    pwsh: add profile

---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,28 @@
+$PSReadLineOptions = @{
+    BellStyle = 'None'
+    CompletionQueryItems = 20
+    EditMode = 'Emacs'
+    PredictionSource = 'HistoryAndPlugin'
+    WordDelimiters = ';:,.[]{}()/\|^&*-=+_''---'
+}
+Set-PSReadLineOption @PSReadLineOptions
+
+if (Get-Command eza -errorAction SilentlyContinue) {
+    Set-Alias ls eza
+    Set-Alias l eza
+    function la { eza -a }
+    function ll { eza -l }
+    function lla { eza -la }
+}
+
+if (Get-Command zoxide -errorAction SilentlyContinue) {
+    Invoke-Expression (& { (zoxide init powershell | Out-String) })
+}
+
+if (Get-Command starship -errorAction SilentlyContinue) {
+    function Invoke-Starship-TransientFunction {
+        &starship module character
+    }
+    Invoke-Expression (&starship init powershell)
+    Enable-TransientPrompt
+}
