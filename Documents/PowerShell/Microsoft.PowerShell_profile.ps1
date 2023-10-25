e2af6c35f7bdfd12aae110c1bb6bbbd8b3039ca6
backwardspy <backwardspy@pigeon.life>
2023-10-25 12:14:08 +0100

    pwsh: fix zoxide not updating its database

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,29 +1,50 @@
 $PSReadLineOptions = @{
-    BellStyle = 'None'
-    CompletionQueryItems = 20
-    EditMode = 'Emacs'
-    PredictionSource = 'HistoryAndPlugin'
-    WordDelimiters = ';:,.[]{}()/\|^&*-=+_''---'
+  BellStyle = 'None'
+  CompletionQueryItems = 20
+  EditMode = 'Emacs'
+  PredictionSource = 'HistoryAndPlugin'
+  WordDelimiters = ';:,.[]{}()/\|^&*-=+_''---'
 }
 Set-PSReadLineOption @PSReadLineOptions
 
-if (Get-Command eza -errorAction SilentlyContinue) {
-    Set-Alias ls eza
-    Set-Alias l eza
-    function la { eza -a }
-    function ll { eza -l }
-    function lla { eza -la }
-    function lt { eza --tree }
+if (Get-Command eza -errorAction SilentlyContinue)
+{
+  Set-Alias ls eza
+  Set-Alias l eza
+  function la
+  { 
+    eza -a 
+  }
+  function ll
+  {
+    eza -l 
+  }
+  function lla
+  { 
+    eza -la 
+  }
+  function lt
+  { 
+    eza --tree 
+  }
 }
 
-if (Get-Command zoxide -errorAction SilentlyContinue) {
-    Invoke-Expression (& { (zoxide init powershell | Out-String) })
+if (Get-Command starship -errorAction SilentlyContinue)
+{
+  function Invoke-Starship-TransientFunction
+  {
+    &starship module character
+  }
+  Invoke-Expression (&starship init powershell)
+  Enable-TransientPrompt
 }
 
-if (Get-Command starship -errorAction SilentlyContinue) {
-    function Invoke-Starship-TransientFunction {
-        &starship module character
-    }
-    Invoke-Expression (&starship init powershell)
-    Enable-TransientPrompt
+# important! zoxide must remain the last thing initialised.
+# https://github.com/ajeetdsouza/zoxide/issues/586
+if (Get-Command zoxide -errorAction SilentlyContinue)
+{
+  Invoke-Expression (& { (zoxide init powershell | Out-String) })
 }
+
+
+
