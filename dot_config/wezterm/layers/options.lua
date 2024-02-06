c81d0dd718b32e98954b6d0d767b7ccc4f510383
backwardspy <backwardspy@pigeon.life>
2024-02-06 14:15:12 +0000

    wezterm: strip config

--- dot_config/wezterm/layers/options.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-function M.apply(config, _)
-	config.initial_cols = 100
-	config.initial_rows = 50
-	print(config.initial_cols, config.initial_rows)
-end
-
-return M
--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,17 +1,76 @@
-local wez = require("wezterm")
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
 
-local config = {}
-if wez.config_builder then
-	config = wez.config_builder()
+-- use pwsh on windows
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+  config.launch_menu = {}
+  local pwsh_args = { 'pwsh.exe', '-NoLogo' }
+  config.default_prog = pwsh_args
+  table.insert(config.launch_menu, {
+    label = 'PowerShell',
+    args = pwsh_args,
+  })
 end
 
-local layers = { "aesthetics", "binds", "options", "windows" }
-
-for _, layer in ipairs(layers) do
-	local ok, layer_config = pcall(require, "layers." .. layer)
-	if ok then
-		layer_config.apply(config, wez)
-	end
+-- sync colour scheme with os settings
+local function appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  else
+    return "Dark"
+  end
 end
+
+local function color_scheme()
+  if appearance():find("Dark") then
+    return "Catppuccin Mocha"
+  else
+    return "Catppuccin Latte"
+  end
+end
+
+config.color_scheme = color_scheme()
+local colors = wezterm.color.get_builtin_schemes()[config.color_scheme]
+
+-- ui/tab bar
+local tab_left = wezterm.nerdfonts.ple_lower_right_triangle
+local tab_right = wezterm.nerdfonts.ple_lower_left_triangle
+local tab_transparent = { Color = colors["tab_bar"]["background"] }
+local tab_active_bg = { Color = colors["tab_bar"]["active_tab"]["bg_color"] }
+local tab_active_fg = { Color = colors["tab_bar"]["active_tab"]["fg_color"] }
+local tab_inactive_bg = { Color = colors["tab_bar"]["inactive_tab"]["bg_color"] }
+local tab_inactive_fg = { Color = colors["tab_bar"]["inactive_tab"]["fg_color"] }
+
+local function tabify(s, active, max_width)
+  local bg = active and tab_active_bg or tab_inactive_bg
+  local fg = active and tab_active_fg or tab_inactive_fg
+
+  local lead = wezterm.format({
+    { Background = tab_transparent },
+    { Foreground = bg },
+    { Text = tab_left },
+  })
+
+  local text = wezterm.truncate_right(s, max_width - 4)
+  local body = wezterm.format({
+    { Background = bg },
+    { Foreground = fg },
+    { Text = " " .. text .. " " },
+  })
+
+  local trail = wezterm.format({
+    { Background = tab_transparent },
+    { Foreground = bg },
+    { Text = tab_right },
+  })
+
+  return lead .. body .. trail
+end
+
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+wezterm.on("format-tab-title", function(tab, _, _, _, _, max_width)
+  return tabify(tab.active_pane.title, tab.is_active, max_width)
+end)
 
 return config
